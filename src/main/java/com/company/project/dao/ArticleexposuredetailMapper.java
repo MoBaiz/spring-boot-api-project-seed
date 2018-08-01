b38555775bdebd25b2c409b6f16960cb1faaa9fd
mobaiz <381473354@qq.com>
2018-08-02 00:55:32 +0800

    add api test demo

---
+++ src/main/java/com/company/project/dao/ArticleexposuredetailMapper.java
@@ -0,0 +1,7 @@
+package com.company.project.dao;
+
+import com.company.project.core.Mapper;
+import com.company.project.model.Articleexposuredetail;
+
+public interface ArticleexposuredetailMapper extends Mapper<Articleexposuredetail> {
+}
